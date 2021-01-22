840e6a9946758c8003eccb83853aeec6062ec82e
OnlyGod Ovbije <onlygodovbije@gmail.com>
2021-01-22 15:30:02 +0100

    hamburger menu fade-in-out

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+const hamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector(".header");
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function() {
+    if (header.classList.contains('open')) { //close humburger menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+        console.log('open hamburger : overlay close');
+
+
+
+    } else { // open hamburger menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+        console.log('open hamburger: overlay open');
+
+
+
+    }
+})
